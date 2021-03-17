7d6fb6a6b833795c9bdecd830da084b7b2af1473
Mart <ae_dts@protonmail.com>
2021-03-17 19:15:46 +0100

    "add image rendering"

--- src/Mandelbrot.java
+++ src/Mandelbrot.java
@@ -6,56 +6,86 @@ import java.io.File;
 import javax.imageio.ImageIO;
  */
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class Mandelbrot {
     static int iterations = 0;
-    static int resolution = 1;
-    static int gridBounds = 1;
+    static int resolution = 1000;
+    static int gridBounds = 2;
     static double[][] points;
     static double[] z = {0,0};
 
-    public static void main(String[] args) {
+    static final int rowWidth = (gridBounds* resolution * 2 + 1) ;
+    static BufferedImage image = new BufferedImage(rowWidth,rowWidth,BufferedImage.TYPE_INT_RGB);
+
+    public static void main(String[] args) throws IOException {
 
         double[] c = {-1,5};
         double[] result = iterate(z,c);
-        System.out.println("r: " + result[0] + " | " + " i:" + result[1] );
+        //System.out.println("r: " + result[0] + " | " + " i:" + result[1] );
 
-        //createPoints();
+
+        createPoints();
 
     }
 
-
-
-    public static void createPoints() {
+    public static void createPoints() throws IOException {
         double x = -gridBounds;
         double y = gridBounds;
         final double negativeXBound = x;
 
-        final int rowWidth = (gridBounds* resolution * 2 + 1) ;
-        //System.out.println("row width: " + rowWidth);
+
+        System.out.println("row width: " + rowWidth);
 
         points = new double[rowWidth * rowWidth][2];
         //System.out.println(points.length);
 
+        int column = 0;
+        int row = 0;
+
         for (int i = 1; i <= points.length; i++) {
             double[] coords = {x, y};
-            System.out.println(isMandelbrot(x,y));
+            if (isMandelbrot(x,y)) {
+                Color black = new Color(0,0,0);
+                int rgb = black.getRGB();
+                System.out.println("column: " + column +  " | row: " + row);
+                image.setRGB(column, row, rgb);
+            } else {
+                Color white = new Color(255,255,255);
+                int rgb = white.getRGB();
+                System.out.println("column: " + column +  " | row: " + row);
+                image.setRGB(column,row,rgb);
+            }
             points[i-1] = coords;
 
-            //System.out.println(round(x, 1) + " | " + round(y, 1) + " | I: " + i);
+            //System.out.println(round(x, 1) + " | " + round(y, 1) + " | I: " + (i-1));
             if (i % rowWidth == 0) {
                 y = y - 1.0 / resolution;
                 x = negativeXBound;
+                column = 0;
+                row++;
                 //System.out.println("reset at index " + i + " | " + rowWidth);
             } else {
+                column++;
                 x = x + 1.0 / resolution;
             }
+
+
+
         }
 
-        for (int i = 0; i < points.length; i++) {
-            System.out.println(round(points[i][0], 1 ) + " | " + round(points[i][1], 1) + " | I: ");
+        System.out.println(image.getRGB(5,5));
+        File file = new File("saved.png");
+        ImageIO.write(image, "png", file);
+
+        for (double[] point : points) {
+            //System.out.println(round(point[0], 1) + " | " + round(point[1], 1) + " | I: ");
         }
     }
 
@@ -68,12 +98,12 @@ public class Mandelbrot {
 
 
     static boolean isMandelbrot(double x, double y) {
-        double[] c = {-0.75,0.1};
+        double[] c = {x,y};
 
         double[] result = iterate(z,c);
         if (Double.isInfinite(result[0]) || Double.isInfinite(result[1])) return false;
         double distance = distanceFromOrigin(result[0], result[1]);
-        System.out.println(distance);
+        //System.out.println(distance);
         return distanceFromOrigin(result[0], result[1]) < 2.0;
    }
 
@@ -86,7 +116,7 @@ public class Mandelbrot {
 
         if (Double.isInfinite(result[0]) || Double.isInfinite(result[1])) return result;
 
-        System.out.println("r: " + result[0] + " | " + " i:" + result[1] );
+        //System.out.println("r: " + result[0] + " | " + " i:" + result[1] );
 
         if (iterations < 30) {
             iterations++;
